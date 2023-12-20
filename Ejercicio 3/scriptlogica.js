7fc9d9cc3448957f70cb35a03751dd9c303921ff
Valentina Cedano <valentina.cedano57@gmail.com>
2023-12-20 16:15:33 -0500

    Cambios del workshop

---
+++ Ejercicio 3/scriptlogica.js
@@ -0,0 +1,47 @@
+/*Completar las siguientes tablas de verdad, resolviendo cada 
+expresión lógica desde un algoritmo usando JavaScript y modificando los 
+diferentes valores que toma las variables de proposición p y q:
+*/
+
+// Primera tabla de la verdad
+
+let p,q;
+
+
+p = true;
+q = true;
+console.log("\nPrimera tabla de la verdad\n");
+console.log("---------------------------------")
+console.log("p    |  q   | NOT p| AND q \t");
+console.log(`${p} | ${q} | ${!p}| ${!p && q}`);
+p = true;
+q = false;
+console.log(`${p} | ${q}| ${!p}| ${!p && q}`);
+p = false;
+q = true;
+console.log(`${p}| ${q} | ${!p} | ${!p && q}`);
+p = false;
+q = false;
+console.log(`${p}| ${q}| ${!p} | ${!p && q}`);
+
+console.log("---------------------------------")
+
+// Segunda Tabla de la verdad
+
+console.log("\nSegunda tabla de la verdad\n");
+
+p = true;
+q = true;
+console.log("---------------------------------")
+console.log("NOT p | NOT q | NOT p AND NOT q |\t");
+console.log(`${!p} | ${!q} |   ${!p && !q}         |`);
+p = true;
+q = false; 
+console.log(`${!p} | ${!q}  |   ${!p && !q}         |`);
+p = false;
+q = true;
+console.log(`${!p}  | ${!q} |   ${!p && !q}         |`);
+p = false;
+q = false;
+console.log(`${!p}  | ${!q}  |   ${!p && !q}          |`);
+console.log("---------------------------------")
